37728a7064adc5541dd9e51d1dc1b4d3811840a1
WenceslaoFB <89049514+WenceslaoFB@users.noreply.github.com>
2021-08-18 16:54:32 -0300

    Add files via upload

---
+++ GccApplication3/Display.c
@@ -0,0 +1,8 @@
+/*
+ * Display.c
+ *
+ * Created: 18/8/2021 16:38:43
+ *  Author: Admin
+ */ 
+
+#include <Display.h>
---
+++ GccApplication3/Display.h
@@ -0,0 +1,30 @@
+/*
+ * Display.h
+ *
+ * Created: 18/8/2021 16:37:18
+ *  Author: Admin
+ */ 
+
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+#define WIDTH 128 /*!<Descripion de width*/
+#define HEIGHT 64 /**/
+
+/** titulo breve
+* Descripcion mas detallada
+*/
+void refreshDisplay();
+
+/** titulo breve
+* Descripcion mas detallada
+*
+* param [in] param1: descripcion
+* param [out] param2: descripcion
+*
+* return explicar que retorna
+*/
+int refreshDisplay(uint8_t param1, uint8_t *param2)
+
+#endif /* DISPLAY_H_ */
